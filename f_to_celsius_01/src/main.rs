5f79bcb803ee3bba8d01f70630ffd857bf6019e5
Traian <traian30y@yahoo.co.uk>
2023-01-22 11:42:42 +0000

    fahren to celsius v1

--- f_to_celsius_01/src/main.rs
+++ f_to_celsius_01/src/main.rs
@@ -1,3 +1,44 @@
+// convert temp from f to c
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut temp_fahren = String::new();
+        println!("please input the temp in Fahrenheit: ");
+        io::stdin()
+            .read_line(&mut temp_fahren)
+            .expect("failed to read line!");
+        let temp_fahren: i32 = match temp_fahren.trim().parse() {
+            Ok(temp) => temp,
+            Err(_) => continue,
+        };
+        println!("The temp in Fahrenheit is: {}", temp_fahren);
+        let celsius = (temp_fahren - 32) * 5 / 9;
+        println!("The temp in celsius is: {}", celsius);
+        let continue_game = quitting();
+        if continue_game == false {
+            break;
+        } else {
+            continue;
+        }
+    }
+}
+
+fn quitting() -> bool {
+    loop {
+        println!("Do you want to continue? y/n: ");
+        let mut quitting = String::new();
+        io::stdin()
+            .read_line(&mut quitting)
+            .expect("failed to read line!");
+        let quitting = quitting.trim_end().to_lowercase();
+        let quitting_chars: Vec<char> = quitting.chars().collect();
+        let quitting_char = quitting_chars[0];
+        if quitting_char == 'y' {
+            return true;
+        } else if quitting_char == 'n' {
+            return false;
+        } else {
+            println!("Invalid input! y/n only ")
+        }
+    }
 }
