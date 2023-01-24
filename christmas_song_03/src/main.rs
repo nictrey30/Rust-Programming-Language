29c7077f7ebb1a1c2df5816819c6bef0b7614539
Traian <traian30y@yahoo.co.uk>
2023-01-24 11:06:57 +0000

    christmas_song_03

--- christmas_song_03/src/main.rs
+++ christmas_song_03/src/main.rs
@@ -1,6 +1,9 @@
 // Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” taking advantage of the repetition in the song.
 
 fn main() {
+    //   String is the dynamic heap string type, like Vec: use it when you need to own or modify your string data.
+
+    // str is an immutable1 sequence of UTF-8 bytes of dynamic length somewhere in memory. Since the size is unknown, one can only handle it behind a pointer. This means that str most commonly2 appears as &str: a reference to some UTF-8 data, normally called a "string slice" or just a "slice".
     let days: [&str; 12] = [
         "a partridge in a pear tree",
         "two turtle doves",
