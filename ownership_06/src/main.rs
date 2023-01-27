24ed812c51b6cd4942ec09b2725e7b8125e8be31
Traian <traian30y@yahoo.co.uk>
2023-01-27 09:11:48 +0000

    update

---
+++ ownership_06/src/main.rs
@@ -0,0 +1,7 @@
+// Ownership rules
+// Each value in Rust has a varible that's called its owner
+// There can only be one owner at a time
+// When the owner goes out of scope, the value will be dropped
+
+// a scope is a range within a program for wich an item is valid
+fn main() {}
