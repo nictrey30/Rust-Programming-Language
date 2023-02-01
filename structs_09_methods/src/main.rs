7e2d34e543e9138f4c5c3e829563bb9dcf457827
Traian <traian30y@yahoo.co.uk>
2023-02-01 14:15:37 +0000

    struct_methods update

--- structs_09_methods/src/main.rs
+++ structs_09_methods/src/main.rs
@@ -4,7 +4,14 @@ struct Rectangle {
     height: u32,
 }
 // define a function within context of Rectangle, we start an impl (implementation) block
+// methods are defined within the context of a struct, enum or trait object, and their first parameter is always &self, which rerpesents the instance of the struct the method is being called on.
+
 impl Rectangle {
+    // Rust knows the type of self is rectangle due to this method's being inside the impl Rectangle context
+
+    // we've chosen &self here because we don't want to take ownership, and we just want to read the data in the struct, notwrite to it.
+    // if we wanted to change the instance that we've called the method on as part of what the method does, we'd use &mut self as the first parameter
+    // using self as param is rare, use case: prevent the caller from using the original instance, as the method transforms self into something else
     fn area(&self) -> u32 {
         self.width * self.height
     }
