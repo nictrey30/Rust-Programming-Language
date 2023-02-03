b78d6591f311a1f115423aa6cb6ed25d3d2ac775
Traian <traian30y@yahoo.co.uk>
2023-02-03 13:24:14 +0000

    structs summary

--- structs_09_03_methods/src/main.rs
+++ structs_09_03_methods/src/main.rs
@@ -24,13 +24,24 @@ fn main() {
     // This makes sense because we only need to read rect2(rather than write), and we want main to retain ownership of rect2, so we can use it again after calling can_hold method
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    let sq = Rectangle::square(30);
+    println!("A square: {:?}", sq);
 }
-
+// Associated functions - within impl block we are allowed to define functions that don't take self as a parameter.
+// associated functions are often used for constructors that will return a new instance of the struct
 impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
+    }
+    // associated function
+    // to call this function we use :: syntax
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
     }
 }
