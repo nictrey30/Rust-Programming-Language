cd37abecbcd50bfc8fab8107aff02c63e20dfa40
Traian <traian30y@yahoo.co.uk>
2023-02-04 01:05:13 -0800

    update thinkpad

--- structs_09/src/main.rs
+++ structs_09/src/main.rs
@@ -1,8 +1,9 @@
-struct User {
-    // used String instead of &str, because we want the instances of this struct to own all of its data and for that data to be valid as long as the entire struct is valid
+// used String instead of &str, because we want the instances of this struct to own all of its data and for that data to be valid as long as the entire struct is valid
 
-    // it is possible for structs to store references to data owned by someone else, but to do so require the use of 'lifetimes'
-    // 'lifetimes' ensure that the data referenced by a struct is valid for as long as the struct is.
+// it is possible for structs to store references to data owned by someone else, but to do so require the use of 'lifetimes'
+// 'lifetimes' ensure that the data referenced by a struct is valid for as long as the struct is.
+#[derive(Debug)]
+struct User {
     username: String,
     email: String,
     sign_in_count: u64,
@@ -22,7 +23,7 @@ fn main() {
         String::from("someusername123"),
     );
     // struct update syntax ..
-    let _user2 = User {
+    let user2 = User {
         username: String::from("anotherusername567"),
         email: String::from("another@example.com"),
         ..user1
@@ -35,6 +36,8 @@ fn main() {
     let _black = Color(0, 0, 0);
     let _origin = Point(0, 0, 0);
     // black and origin are diffrent types, because they're instances of diffrent tuple structs
+    println!("user1: {:?}", user1);
+    println!("user2: {:?}", user2);
 }
 fn build_user(email: String, username: String) -> User {
     User {
