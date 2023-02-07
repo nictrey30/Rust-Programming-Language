d34e0a68cd8dc6d4a7f99d1dd2e60c5e0448cbfe
Traian <traian30y@yahoo.co.uk>
2023-02-07 13:04:13 +0000

    update

---
+++ coll_vectors_11_01/src/main.rs
@@ -0,0 +1,10 @@
+// common collections
+// - vector - allows you to store a variable number of values next to each other
+// - string - is a collection of characters
+// -hash map - allows you to associate a value with a particular key
+
+// vectors allows you to store more than one value in a single data structure that puts all the values next to each other in memory
+// vectors can only store values of the same type
+fn main() {
+    let v: Vec<i32> = Vec::new();
+}
