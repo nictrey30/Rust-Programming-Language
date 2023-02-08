ee304c09abdd304207c042845d091a4d07bc1637
Traian <traian30y@yahoo.co.uk>
2023-02-08 12:03:15 +0000

    collection - strings

---
+++ coll_strings_02/src/main.rs
@@ -0,0 +1,9 @@
+// Collection - Strings
+fn main() {
+    // let mut s = String::new();
+    let data = "initial contents";
+    let s = data.to_string();
+
+    // or we can use string::from()
+    let s = String::from("hello world");
+}
